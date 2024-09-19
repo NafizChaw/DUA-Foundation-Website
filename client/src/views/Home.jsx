518f498adcf27d700923a1168582e4936a402b50
Nafiz Chawdhury <98878310+NafizChaw@users.noreply.github.com>
2024-09-19 00:43:47 -0400

    Reapply "Bootstrap implementation and donation page"
    This reverts commit 008969a5c375125c0b66c3bc79f8e1015e591d33.

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,20 +1,41 @@
-import Header from "./shared/Header.jsx";
-import Sidebar from "./Sidebar.jsx";
-import './styles/Home.css'
+import React from 'react';
+import Header from './shared/Header.jsx';
+import Sidebar from './Sidebar.jsx';
+import './styles/Home.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Home(){
+function Home() {
     return (
-        <div className="App">
+        <div className="container-fluid">
             {/* Title for the landing page */}
-            <div className="landing-page-title">
-                <h1>Dua Foundation</h1>
-                {/* You can add additional styling or text here */}
+            <div className="row justify-content-center landing-page-title">
+                <div className="col-12 text-center">
+                    <h1>Dua Foundation</h1>
+                </div>
             </div>
 
-            <Header/> {/* Logo is displayed here */}
-            <Sidebar/>
+            {/* Header */}
+            <div className="row">
+                <div className="col-12">
+                    <Header /> {/* Logo is displayed here */}
+                </div>
+            </div>
+
+            <div className="row">
+                {/* Sidebar (Visible on larger screens) */}
+                <div className="col-md-4 col-12">
+                    <Sidebar />
+                </div>
+
+                {/* Main Content */}
+                <div className="col-md-8 col-12">
+                    <div className="content text-center">
+                        <p></p>
+                    </div>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Home;
